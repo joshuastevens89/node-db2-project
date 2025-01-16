8986c33c349ce01a7447acbc50c9c93c6c021545
Joshua Stevens <e2855845@student.dcccd.edu>
2025-01-16 10:14:29 -0600

    fleshed out seed file with mock car data

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,40 @@
 // STRETCH
+const cars = [
+    {
+        vin: "11111111111111111",
+        make: "toyota",
+        model: "prius",
+        mileage: 215000,
+        title: "clean",
+        transmission: "manual",
+    },
+
+    {
+        vin: "11111111111111111",
+        make: "toyota",
+        model: "corolla",
+        mileage: 115000,
+        title: "salvage",
+        transmission: "manual",
+    },
+
+    {
+        vin: "11111111111111111",
+        make: "toyota",
+        model: "prius",
+        mileage: 215000,
+        title: "clean",
+        transmission: "manual",
+    }
+]
+
+// exports.seed = function(knex) {
+//     return knex("cars").truncate().then(() => {
+//         return knex("cars").insert(cars)
+//     })
+// }
+
+exports.seed = async function (knex) {
+    await knex("cars").truncate()
+    await knex("cars").insert(cars)
+}
