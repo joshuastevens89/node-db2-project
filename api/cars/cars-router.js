fc8ac25a82fd31b642fa1b63148d8f986a2b70fe
Joshua Stevens <e2855845@student.dcccd.edu>
2025-01-16 11:58:05 -0600

    built getAll in model and router.get in router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,8 +1,19 @@
 // DO YOUR MAGIC
 const express = require('express')
+const Car = require('./cars-model')
 
 const router = express.Router()
 
+router.get('/', async (req, res, next) => {
+    try {
+        const cars = await Car.getAll()
+        res.json(cars)
+        res.json('getting all cars')
+
+    } catch (err) {
+        next(err)
+    }
+})
 
 router.get('/', async (req, res, next) => {
     res.json('getting all cars')
